e6de2549da086c3a3229f6a3f664610fba00e756
David Shilling <david.c.shilling@gmail.com>
2022-01-16 20:46:13 -0500

    Initial Commit

---
+++ PlayoffLeaderboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlayoffLeaderboard
+//
+//  Created by David Shilling on 1/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlayoffLeaderboard/PlayoffLeaderboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayoffLeaderboardApp.swift
+//  PlayoffLeaderboard
+//
+//  Created by David Shilling on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct PlayoffLeaderboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
