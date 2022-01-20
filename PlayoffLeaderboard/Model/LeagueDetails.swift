168827d9f0613f6409be9d4d729223933b060b37
David Shilling <david.c.shilling@gmail.com>
2022-01-19 19:36:13 -0500

    add model for LeagueDetails

---
+++ PlayoffLeaderboard/Model/LeagueDetails.swift
@@ -0,0 +1,26 @@
+//
+//  LeagueDetails.swift
+//  PlayoffLeaderboard
+//
+//  Created by David Shilling on 1/19/22.
+//
+
+import Foundation
+
+// MFL API object
+struct LeagueDetailsResponse: Codable {
+    var league: LeagueDetails
+}
+
+struct LeagueDetails: Codable {
+    var id: String
+    var name: String
+    var baseURL: String
+    var franchise: Franchises
+}
+
+typealias Franchises = [Franchise]
+struct Franchise: Codable {
+    var id: String
+    var name: String
+}
